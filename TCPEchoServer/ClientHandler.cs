7d155842999316b6a3cc3d1ef5f2803e594b249a
move <move@zealand.dk>
2025-02-07 11:33:25 +0100

    Add project files.

---
+++ TCPEchoServer/ClientHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPEchoServer
+{
+    public class ClientHandler
+    {
+        public static void HandleClient(TcpClient client)
+        {
+            Console.WriteLine(client.Client.RemoteEndPoint);
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+
+            string? message = reader.ReadLine();
+            Console.WriteLine(message);
+            writer.WriteLine(message);
+            writer.Flush();
+
+            client.Close();
+        }
+    }
+}
---
+++ TCPEchoServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using TCPEchoServer;
+
+Console.WriteLine("TCP Server");
+int port = 7;
+TcpListener listener = new TcpListener(
+    IPAddress.Any, port);
+listener.Start();
+
+while (true)
+{
+    TcpClient client = listener.AcceptTcpClient();
+    Task.Run(() => ClientHandler.HandleClient(client));
+}
+listener.Stop();
