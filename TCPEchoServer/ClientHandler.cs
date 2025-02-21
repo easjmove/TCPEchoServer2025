9d90c0edcbd9220571c8651c3f91179d1f296390
move <move@zealand.dk>
2025-02-21 11:36:58 +0100

    added most of the class protocol

--- TCPEchoServer/ClientHandler.cs
+++ TCPEchoServer/ClientHandler.cs
@@ -10,6 +10,7 @@ namespace TCPEchoServer
     {
         public static void HandleClient(TcpClient client)
         {
+            List<string> options = new List<string>() { "4", "6", "8", "10", "12", "20" };
             Console.WriteLine(client.Client.RemoteEndPoint);
             NetworkStream ns = client.GetStream();
             StreamReader reader = new StreamReader(ns);
@@ -26,13 +27,40 @@ namespace TCPEchoServer
                     writer.WriteLine(DateTime.Now.ToString("dd/MM/yy - HH:mm"));
                     writer.Flush();
                 }
+                else if (message == "ToUpper")
+                {
+                    writer.WriteLine("write to convert to uppercase");
+                    writer.Flush();
+                    string? messageToConvert = reader.ReadLine();
+                    messageToConvert = messageToConvert.ToUpper();
+                    writer.WriteLine(messageToConvert);
+                    writer.Flush();
+                }
+                else if (message == "RollDice")
+                {
+                    writer.WriteLine("which die 4/6/8/10/12/20");
+                    writer.Flush();
+                    string number = reader.ReadLine();
+                    
+                    if (options.Contains(number))
+                    {
+                        int maxInt = int.Parse(number);
+                        Random random = new Random();
+                        int result = random.Next(1, maxInt + 1);
+                        writer.WriteLine(result);
+                        writer.Flush();
+                    } else
+                    {
+                        writer.WriteLine("Du er en hat!");
+                        writer.Flush();
+                    }
+                }
                 else if (message == "close")
                 {
                     writer.WriteLine("connection closed");
                     writer.Flush();
                     isRunning = false;
                 }
-
             }
             client.Close();
         }
